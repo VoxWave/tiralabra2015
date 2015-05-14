cdda0d372dec17f03706abd19cb50dd55169fb7c
VoxWave <victor_bankowski@hotmail.com>
2015-05-14 19:57:59 +0300

    suunnittelutyötä tehty.

---
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/generator/MazeGenerator.java
@@ -0,0 +1,14 @@
+package com.unknownpotato.dungeon.generator;
+
+import java.util.function.Consumer;
+
+import com.unknownpotato.dungeon.Level;
+
+public class MazeGenerator implements Consumer<Level> {
+
+	@Override
+	public void accept(Level level) {
+		// TODO Auto-generated method stub		
+	}
+
+}
