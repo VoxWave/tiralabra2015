0804ae22318a724f5928c1adb0d35f43d3df33f2
VoxWave <victor_bankowski@hotmail.com>
2015-05-15 17:51:32 +0300

    TUI aloitettu

---
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/ui/TextUI.java
@@ -0,0 +1,16 @@
+package com.unknownpotato.dungeon.ui;
+
+import com.unknownpotato.dungeon.Level;
+
+public class TextUI {
+	
+	private Level level;
+	
+	public TextUI() {
+	}
+	
+	public void start() {
+		
+	}
+
+}
