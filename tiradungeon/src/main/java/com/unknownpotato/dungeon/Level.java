6ab8c8c68de29b7ea7da5636c4748940a352683a
VoxWave <victor_bankowski@hotmail.com>
2015-05-15 17:40:01 +0300

    Level täytetään nyt seinällä. Tileen lisätty gettereitä ja settereitä

--- tiradungeon/src/main/java/com/unknownpotato/dungeon/Level.java
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/Level.java
@@ -5,13 +5,28 @@ import java.util.function.Consumer;
 public class Level {
 	
 	private Tile[][] tiles;
+	private int width;
+	private int height;
 	
 	public Level(int width, int height) {
 		this.tiles = new Tile[width][height];
+		for(int x = 0; x<width; x++) {
+			for(int y = 0; x<height; y++) {
+				this.tiles[x][y] = new Tile(TileType.WALL);
+			}
+		}
 	}
 	
 	public Level apply(Consumer<Level> generator){
 		return this;
 	}
-
+	
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+	
 }
