77ee0550c8c0ccdde68d0f2c0afaf1254392eec1
VoxWave <victor_bankowski@hotmail.com>
2015-05-29 10:50:30 +0300

    node done in queue

--- tiradungeon/src/main/java/com/unknownpotato/dungeon/util/Queue.java
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/util/Queue.java
@@ -10,21 +10,51 @@ import com.unknownpotato.dungeon.util.enums.Direction;
  * @param <T>
  */
 public class Queue<T> {
-	
+	/**
+	 * Nodes are used as a building block for the Queue.
+	 * 
+	 * @author VoxWave
+	 *
+	 * @param <T>
+	 */
 	class Node<T> {
 		private Node next;
 		private Node previous;
 		private T value;
 		
+		public Node(T value) {
+			this.value = value;
+		}
 		
+		public Node getNext() {
+			return next;
+		}
+		
+		public Node getPrev() {
+			return previous;
+		}
+		
+		public T getValue() {
+			return value;
+		}
+		
+		public void setNext(Node node) {
+			next = node;
+		}
+		
+		public void setPrev(Node node) {
+			previous = node;
+		}
+		
+		public void setValue(T data) {
+			value = data;
+		}
 	}
 	
-	private T[] data;
 	private Node first;
 	private Node last;
 	
 	public Queue() {
-		data = (T[])new Object[8];
 		
 	}
 
