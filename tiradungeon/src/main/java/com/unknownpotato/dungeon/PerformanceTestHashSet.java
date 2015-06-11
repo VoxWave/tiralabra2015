24047cda6997a4c85607ddd740f2f61bd6f15aa9
VoxWave <victor_bankowski@hotmail.com>
2015-06-11 22:22:11 +0300

    stacking pushin vertailu dequen pushiin

---
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/PerformanceTestHashSet.java
@@ -0,0 +1,9 @@
+package com.unknownpotato.dungeon;
+
+public class PerformanceTestHashSet {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/PerformanceTestQueueAndStack.java
@@ -0,0 +1,69 @@
+package com.unknownpotato.dungeon;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Random;
+
+import com.unknownpotato.dungeon.util.Queue;
+import com.unknownpotato.dungeon.util.Stack;
+
+public class PerformanceTestQueueAndStack {
+
+	private static void jvmWarmup() {
+		System.gc();
+		for(int i = 0; i < 100; i++) {
+			dequePushBenchmark(i+1*100, i);
+			stackPushBenchmark(i+1*100, i);
+		}
+		System.gc();
+	}
+	
+	public static void main(String[] args) {
+		StringBuilder builder = new StringBuilder();
+		Random rand = new Random();
+		Queue<Integer> queue = new Queue<>();
+		long seed = rand.nextLong();
+		int iterations = 2000000;
+		
+		jvmWarmup();
+		
+		long time = 0;
+		for(int i = 0; i < 100; i++){
+			time += dequePushBenchmark(iterations, seed);
+		}
+		System.out.println(time/100.0);
+//		System.out.println(dequePushBenchmark(iterations, seed));
+
+		
+
+		
+		
+	}
+
+	private static long dequePushBenchmark(int iterations, long seed) {
+		Random rand = new Random(seed);
+		Deque<Integer> deque = new ArrayDeque<>();
+		long startTimedq = System.currentTimeMillis();
+		for(int i = 0; i < iterations; i++) {
+			int elem = rand.nextInt();
+			deque.push(elem);
+		}
+		long stopTimedq = System.currentTimeMillis();
+		long dequeTime = stopTimedq - startTimedq;
+		return dequeTime;
+	}
+
+	private static long stackPushBenchmark(int iterations, long seed) {
+		Random rand = new Random(seed);
+		Stack<Integer> stack = new Stack<>();
+		long startTime = System.currentTimeMillis();
+		for(int i = 0; i < iterations; i++) {
+			int elem = rand.nextInt();
+			stack.push(elem);
+		}
+		long stopTime = System.currentTimeMillis();
+		long stackTime = stopTime-startTime;
+		return stackTime;
+	}
+
+}
