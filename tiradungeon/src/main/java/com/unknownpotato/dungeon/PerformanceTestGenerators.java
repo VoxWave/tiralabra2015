705437a462e8ee61cd06c746f9dcdfce26e835c1
VoxWave <victor_bankowski@hotmail.com>
2015-06-20 17:39:15 +0300

    suorituskykytestauksia generaattoreille tehty

--- tiradungeon/src/main/java/com/unknownpotato/dungeon/PerformanceTestGenerators.java
+++ tiradungeon/src/main/java/com/unknownpotato/dungeon/PerformanceTestGenerators.java
@@ -8,18 +8,19 @@ import com.unknownpotato.dungeon.util.PNGExporter;
 public class PerformanceTestGenerators {
 	
 	public static void main( String[] args ) {
-		RoomGenerator gen2 = new RoomGenerator(4,20, 3, 10000000);
+		RoomGenerator gen2 = new RoomGenerator(4,20, 3, 10);
 		MazeGenerator gen = new MazeGenerator(24,24);
 		DungeonGenerator gen3 = new DungeonGenerator();
 		
-		int x = 60;
+		int x = 300;
     	Level level = new Level(x,x);
     	
     	long startTime = System.currentTimeMillis();
     	level.apply(gen2);
     	long stopTime = System.currentTimeMillis();
+    	System.out.println(level);
     	System.out.println(stopTime - startTime);
-//    	System.out.println(level);
+    	
     }
 
 }
